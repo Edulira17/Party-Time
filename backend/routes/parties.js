f5d8042fb5e605e4f9b2fb1ce213d46b268bd98a
Eduardo Lira <eduardoliracunha17@gmail.com>
2024-11-07 15:14:34 -0400

    get individual

--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -2,6 +2,13 @@ const router = require("express").Router();
 
 const partyController = require("../controllers/partyController");
 
+// create party:
 router.route("/parties").post((req, res) => partyController.create(req, res))
+
+// returns all parties
+router.route("/parties").get((req, res) => partyController.getAll(req, res))
+
+// returns individual parties
+router.route("/parties/:id").get((req, res) => partyController.get(req, res))
 
 module.exports = router;
