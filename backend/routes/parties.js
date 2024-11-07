35b39cb4817408b7a81445a9607058e45d3d9f6c
Eduardo Lira <eduardoliracunha17@hotmail.com>
2024-11-07 17:48:33 -0400

    individual request

--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -3,12 +3,12 @@ const router = require("express").Router();
 const partyController = require("../controllers/partyController");
 
 // create party:
-router.route("/parties").post((req, res) => partyController.create(req, res))
+router.route("/parties").post((req, res) => partyController.create(req, res));
 
 // returns all parties
-router.route("/parties").get((req, res) => partyController.getAll(req, res))
+router.route("/parties").get((req, res) => partyController.getAll(req, res));
 
 // returns individual parties
-router.route("/parties/:id").get((req, res) => partyController.get(req, res))
+router.route("/parties/:id").get((req, res) => partyController.get(req, res));
 
 module.exports = router;
