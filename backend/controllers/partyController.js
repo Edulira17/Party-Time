9cc3cc0a0ff370db41eeef75dc15d93b692e94f9
Eduardo Lira <eduardoliracunha17@gmail.com>
2024-11-07 15:14:18 -0400

    get all

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -3,14 +3,12 @@ const PartyModel = require("../models/Party");
 const checkPartyBudget = (budget, services) => {
   const priceSum = services.reduce((sum, service) => sum + service.price, 0);
 
-  console.log(priceSum, budget)
-
   if (priceSum > budget) {
     return false;
   }
 
   return true;
-}
+};
 
 const partyController = {
   create: async (req, res) => {
@@ -21,22 +19,22 @@ const partyController = {
         description: req.body.description,
         budget: req.body.budget,
         image: req.body.image,
-        services: req.body.services
+        services: req.body.services,
       };
 
       // REGRA DE NEGÓCIO -> BUDGET < VALOR DOS SERVIÇOS != NOVO SERVIÇO
-      if (party.services && !checkPartyBudget(party.budget, party.services)){
-        res.status(406).json({msg: "O seu orçamento é insuficiente!"});
+      if (party.services && !checkPartyBudget(party.budget, party.services)) {
+        res.status(406).json({ msg: "O seu orçamento é insuficiente!" });
         return;
       }
 
-      const response = await PartyModel.create(party)
+      const response = await PartyModel.create(party);
 
-      res.status(201).json({response, msg: "Festa criada com sucesso!"})
+      res.status(201).json({ response, msg: "Festa criada com sucesso!" });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  },
 };
 
-module.exports = partyController
+module.exports = partyController;
